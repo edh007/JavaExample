a255a67324c54ba5049708076ed0324db1a83633
donghoj <edongho007@gmail.com>
2019-07-12 16:47:51 -0700

    Work on event driven control

--- MyGdxGame/core/src/com/mygdx/game/MyGdxGame.java
+++ MyGdxGame/core/src/com/mygdx/game/MyGdxGame.java
@@ -36,6 +36,11 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -43,38 +48,62 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 
-public class MyGdxGame implements ApplicationListener {
+public class MyGdxGame implements ApplicationListener, InputProcessor {
 	private SpriteBatch batch;
-	private Texture texture;
+	private TextureAtlas textureAtlas;
 	private Sprite sprite;
 	private Pixmap pixmap;
+	private int currentFrame = 1;
+	private String currentAtlasKey = new String("0001");
+
+	private float posX, posY;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
-		pixmap = new Pixmap(256, 128, Pixmap.Format.RGBA8888);
-		pixmap.setColor(Color.RED);
-		pixmap.fill();
-		pixmap.setColor(Color.BLACK);
-		pixmap.drawLine(0, 0, pixmap.getWidth()-1, pixmap.getHeight()-1);
-		pixmap.drawLine(0, pixmap.getHeight()-1, pixmap.getWidth()-1, 0);
+		textureAtlas = new TextureAtlas(Gdx.files.internal("data/spritesheet.atlas"));
+		TextureAtlas.AtlasRegion region = textureAtlas.findRegion("0001");
 
-		pixmap.setColor(Color.YELLOW);
-		pixmap.drawCircle(pixmap.getWidth()/2, pixmap.getHeight()/2, pixmap.getHeight()/2 - 1);
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 
-		//texture = new Texture(Gdx.files.internal("data/jet.png"));
-		texture = new Texture(pixmap);
+		sprite = new Sprite(region);
+		posX = w/2 - sprite.getWidth()/2;
+		posY = h/2 - sprite.getHeight()/2;
+		sprite.setPosition(posX, posY);
+		sprite.scale(2.5f);
+		Timer.schedule(new Task() {
+			@Override
+			public  void run() {
+				currentFrame++;
+				if(currentFrame > 20)
+					currentFrame = 1;
+				//currentAtlasKey = String.format("%04d", currentFrame);
+				String base = new String();
 
-		pixmap.dispose();
+				if(currentFrame >= 10)
+					base = "00";
+				else
+					base = "000";
 
-		sprite = new Sprite(texture);
+				currentAtlasKey = base + currentFrame;
+
+				sprite.setRegion(textureAtlas.findRegion(currentAtlasKey));
+			}
+		}, 0, 1/30.0f);
+
+		Gdx.input.setInputProcessor(this);
 	}
+
 	@Override
 	public void dispose() {
 		batch.dispose();
-		texture.dispose();
+		textureAtlas.dispose();
 	}
 
 	@Override
@@ -82,12 +111,103 @@ public class MyGdxGame implements ApplicationListener {
 		Gdx.gl.glClearColor(0,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		//controlMouse();
+		sprite.setPosition(posX,posY);
+
 		batch.begin();
-		sprite.setPosition(0, 0);
+//		sprite.setPosition(0, 0);
 		sprite.draw(batch);
-		sprite.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-		sprite.draw(batch);
+//		sprite.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+//		sprite.draw(batch);
 		batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		float moveAmount = 1.0f;
+		if(Gdx.input.isKeyPressed(Keys.CONTROL_LEFT))
+			moveAmount = 10.0f;
+
+		if(keycode == Keys.LEFT)
+			posX-=moveAmount;
+		if(keycode == Keys.RIGHT)
+			posX+=moveAmount;
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if(button == Buttons.LEFT){
+			posX = screenX - sprite.getWidth()/2;
+			posY = Gdx.graphics.getHeight() - screenY - sprite.getHeight()/2;
+		}
+		if(button == Buttons.RIGHT){
+			posX = Gdx.graphics.getWidth()/2 - sprite.getWidth()/2;
+			posY = Gdx.graphics.getHeight()/2 - sprite.getHeight()/2;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+
+	public void controlMouse()
+	{
+		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+		{
+			sprite.setPosition(Gdx.input.getX() - sprite.getWidth()/2,
+					Gdx.graphics.getHeight() - Gdx.input.getY() - sprite.getHeight()/2);
+		}
+		if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT))
+		{
+			sprite.setPosition(Gdx.graphics.getWidth()/2 -sprite.getWidth()/2,
+					Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
+		}
+	}
+
+	public void controlKeyboard()
+	{
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+		{
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(-1f);
+			else
+				sprite.translateX(-10f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+		{
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(1f);
+			else
+				sprite.translateX(10f);
+		}
 	}
 
 	@Override
